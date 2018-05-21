7240c2400b642642dfe8889e3fd1b8ad91c9716a
WangKe233 <2195000850@qq.com>
2018-05-21 15:45:46 +0800

    用shell指令批量生成了30篇博客,方便后面采用分页显示页面.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
+from django.core import paginator
 from django.shortcuts import render_to_response,get_object_or_404
-
+from django.core.paginator import  Paginator
 # Create your views here.
 from blog.models import Blog,BlogType
 
 
 def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list,10)  # 每10页进行分页
+    page_num = request.GET.get('page',1)   #拿到GET请求的page,默认显示第一页,这里设置的首页显示前10条
+    page_of_blogs = paginator.get_page(page_num)  #得到第几页的博客列表 ,
+
     context = {}
+    # context['blogs'] =page_of_blogs.object_list
+    context['page_of_blogs'] =page_of_blogs
     context['blog_types'] = BlogType.objects.all()
-    context['blogs'] = Blog.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 
